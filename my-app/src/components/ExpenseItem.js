04d325b8e587fecc6f401c5fe6d007927a31e5a6
Trent Livingston <trentlivi@outlook.com>
2021-10-10 09:52:41 -0600

    added some components and more styling to my react spending tracker

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
 import "./Expenseitem.css";
-function ExpenseItem() {
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
-            <div>Date</div>
-            <div className="expense-item__Description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+        <div className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
-};
+}
 
 export default ExpenseItem;
